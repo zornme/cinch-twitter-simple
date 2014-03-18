00eb08ad2c53d7a0327079b82380923b60169acf
Matt Zorn <zornme@gmail.com>
2014-03-18 13:36:53 -0400

    rename test method for consistency

--- test/test.rb
+++ test/test.rb
@@ -2,7 +2,7 @@ require 'test/unit'
 require 'cinch/twitter/simple'
 
 class Cinch::Twitter::Simple::Test < Test::Unit::TestCase
-    def test_gets_tweet
+    def test_get_tweet
         # thanks wolfpupy
         assert_equal Cinch::Twitter::Simple.get_tweet(445481525144846336).text,
             "going to astrally project myself the hell out of this god damn town"
