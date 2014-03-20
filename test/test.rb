12031c218aa06cab950f6343548109ce219ac03d
Matt Zorn <zornme@gmail.com>
2014-03-20 12:38:44 -0400

    create twitter client once on initialize

--- test/test.rb
+++ test/test.rb
@@ -1,10 +1,18 @@
 require 'test/unit'
+require 'cinch'
 require 'cinch/twitter/simple'
 
 class Cinch::Twitter::Simple::Test < Test::Unit::TestCase
+    def setup
+        @bot = Cinch::Bot.new do
+            self.loggers.clear
+        end
+        @plugin = Cinch::Twitter::Simple.new(@bot)
+    end
+
     def test_get_tweet
         # thanks wolfpupy
-        assert_equal Cinch::Twitter::Simple.get_tweet(445481525144846336).text,
+        assert_equal @plugin.get_tweet(445481525144846336).text,
             "going to astrally project myself the hell out of this god damn town"
     end
 end
