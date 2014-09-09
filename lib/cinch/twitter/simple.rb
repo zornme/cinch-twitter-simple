36a35e798ef68e9651aafe2288887124b5a99d9f
Matt Zorn <zornme@gmail.com>
2014-09-08 23:48:20 -0400

    backwards compat

--- lib/cinch/twitter/simple.rb
+++ lib/cinch/twitter/simple.rb
@@ -13,8 +13,8 @@ module Cinch
                 super
 
                 @twitter_client = ::Twitter::REST::Client.new do |config|
-                    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-                    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+                    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY'] || ENV['TWITTER_KEY']
+                    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET'] || ENV['TWITTER_SECRET']
                     config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
                     config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
                 end
