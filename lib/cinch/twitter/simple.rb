7dd5e20130e77c564e0a562797bf3f72a9918020
Matt Zorn <zornme@gmail.com>
2014-09-09 00:10:48 +0000

    use tokens as well

--- lib/cinch/twitter/simple.rb
+++ lib/cinch/twitter/simple.rb
@@ -13,8 +13,10 @@ module Cinch
                 super
 
                 @twitter_client = ::Twitter::REST::Client.new do |config|
-                    config.consumer_key    = ENV['TWITTER_KEY']
-                    config.consumer_secret = ENV['TWITTER_SECRET']
+                    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+                    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+                    config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+                    config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
                 end
             end
 
